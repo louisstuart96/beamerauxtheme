2951025d730f3a9144e474c1816dc7d9f3ec7f68
louisstuart96 <louisstuart96@gmail.com>
2018-12-17 16:45:40 +0800

    Prepare for publishing to CTAN

--- build.lua
+++ build.lua
@@ -5,8 +5,6 @@ sourcefiledir = maindir .. "/themes"
 docfiledir = maindir .. "/doc"
 
 sourcefiles = {"*.sty"}
--- docfiles = {"*.tex"}
--- demofiles = {"*.pdf"}
 cleanfiles = {"*.zip"}
 textfiles = {"*.md", "LICENSE"}
 unpackfiles = {}
@@ -15,37 +13,6 @@ typesetfiles = {"*.tex"}
 packtdszip = true
 flatten = false
 
--- examplelist = {
---     inner = {"simplelines"},
---     outer = {"sidebarwithminiframes","splitwithminiframes","twolines"}
--- }
-
--- function typeset_demo_tasks()
---     local errorlevel = 0
---     local demoname = "beamerauxthemeexample.tex"
---     for type, themelist in pairs(examplelist) do
---         local typename = tostring(type)
---         for _, themename in pairs(themelist) do
---             local texcommand = "\"\\def\\" .. typename .. "themename{" .. themename .. "}\\input{" .. demoname .. "}\""
---             local jobname = "beamerauxthemeexample-" .. typename .. "-" .. themename
---             errorlevel = runtool(
---                 "",
---                 ".",
---                 "TEXINPUTS",
---                 "pdflatex -interaction=nonstopmode -jobname=" .. jobname .. " " .. texcommand
---             ) + runtool(
---                 "",
---                 ".",
---                 "TEXINPUTS",
---                 "pdflatex -interaction=nonstopmode -jobname=" .. jobname .. " " .. texcommand
---             )
---             if errorlevel~=0 then
---                 return errorlevel
---             end
---         end
---     end
---     return 0
--- end
 
 kpse.set_program_name("kpsewhich")
 if not release_date then
